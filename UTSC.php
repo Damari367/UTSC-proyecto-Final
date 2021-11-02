3ce47e60c83b3aa83e6085dbf8d58b4dd365e291
Damaris Torres <torresdamaris338@gmail.com>
2021-11-01 20:49:35 -0600

    Agrego Inicio

---
+++ UTSC.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="estilos/style.css">
+  <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-success navbar-dark">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="UTSC.php"><img src="img/Captura.PNG" alt=""><br> Universidad Tecnologica Santa Catarina</a>
+      </li>
+   
+      <li class="nav-item">
+      <br>
+      <a class="nav-link" href="#"></a>
+      </li>
+    
+      <li class="nav-item">
+      <br>
+        <a class="nav-link" href="#"></a>
+      </li>
+      <li class="nav-item">
+      <br>
+        <a class="nav-link disabled" href="#"></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<br>
+<br>
+<br>
+
+<!-- Carousel -->
+<div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+  <!-- Indicators/dots -->
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+    
+  </div>
+  
+  <!-- The slideshow/carousel -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <center><img src="img/img1.jpg" alt="Los Angeles" class="d-block" ></center>
+      <div class="carousel-caption">
+        <h3>UTSC</h3>
+        <p>Bienvenidos!</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+    <center><img src="img/img2.jpg" alt="Chicago" class="d-block" ></center>
+      <div class="carousel-caption">
+        <br>
+        <p>AVISO!</p>
+      </div> 
+    </div>
+    
+  </div>
+  <!-- Left and right controls/icons -->
+  <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </button>
+</div>
+
+<div class="container p-4 my-4 bg-success text-white" > 
+ <h1>Modelo de Inclusión Educativa y Laboral</h1>
+  <h6>La Universidad Tecnológica Santa Catarina, a través de la Coordinación de
+  Inclusión Educativa, gestiona y coordina todos los apoyos que se proporcionan de manera integral a los alumnos con discapacidad durante el periodo de su estancia en esta Universidad, 
+ tomando en cuenta las necesidades específicas de cada uno de ellos.<h6>
+
+<h6>
+<br>
+<br>
+
+</div>
+
+<center><img src="img/img5.PNG" alt=""></center>
+<nav class="navbar navbar-expand-sm bg-success navbar-dark"></nav>
+</body>
+</html>
